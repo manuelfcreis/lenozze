7da3dfc545b4723310580fc96a049be87c507e4c
Manuel Costa Reis <manuelfcreis@gmail.com>
2019-06-20 10:39:58 +0100

    center list items

--- src/list.js
+++ src/list.js
@@ -3,11 +3,11 @@ import React from "react";
 const list = [
     ['Serviço', require('./images/lista/servico.png'), (<p>Test</p>)],
     ['Faqueiro', require('./images/lista/faqueiro.jpg'), 'cutipol'],
-    ['Copos', require('./images/lista/copos.jpg'), 'cutipol'],
-    ['Copos Cocktail', require('./images/lista/cocktail.jpg'), (<p>Test <strong>2</strong></p>)],
-    ['Kitchen Aid', require('./images/lista/kitchen.jpg'), 'payment'],
-    ['Blender', require('./images/lista/blender.jpg'), 'payment'],
-    ['Food Processor', require('./images/lista/processor.jpg'), 'payment'],
+    ['Copos', require('./images/lista/copos.jpeg'), 'cutipol'],
+    ['Copos Cocktail', require('./images/lista/cocktail.jpeg'), (<p>Test <strong>2</strong></p>)],
+    ['Kitchen Aid', require('./images/lista/kitchen.png'), 'payment'],
+    ['Blender', require('./images/lista/blender.png'), 'payment'],
+    ['Food Processor', require('./images/lista/processor.png'), 'payment'],
   ]
   
 export default list
