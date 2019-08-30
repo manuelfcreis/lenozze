115b313c3001e8f83002e1b804680c3323ebd0b4
Manuel Costa Reis <manuelfcreis@gmail.com>
2019-08-30 10:10:28 +0100

    new gifts

--- src/list.js
+++ src/list.js
@@ -6,11 +6,11 @@ const list = [
   ['copos cocktail', require('./images/lista/cocktail.png'), 360], // 15€ copo, 4 copos conjunto, 6 conjuntos
   ['6 cadeiras de jantar', require('./images/lista/cadeiras.png'), 'oferecido'], // 150 * 6
   ['candeeiro de tecto', require('./images/lista/candeeiro.png'), 480], // flos 480
-  ['candeeiros de mesa', require('./images/lista/candeeiros.png'), 160], // flos 480
+  ['candeeiros de mesa', require('./images/lista/candeeiros.png'), 'oferecido'], // flos 480
   ['robô aspirador', require('./images/lista/robot.jpg'), 'oferecido'],
   ['batedeira ', require('./images/lista/kitchen.png'), 'oferecido'],
   ['liquidificadora', require('./images/lista/blender.png'), 150],
-  ['processador de comida', require('./images/lista/processor.png'), 150],
+  ['processador de comida', require('./images/lista/processor.png'), 'oferecido'],
 ]
 
 export default list
