a05bec94ffaf6e8f9387202ebe5964dea63110dd
Manuel Costa Reis <manuelfcreis@gmail.com>
2019-09-17 18:03:07 +0100

    copos oferecidos

--- src/list.js
+++ src/list.js
@@ -2,7 +2,7 @@ const list = [
   ['viagem ao japão', require('./images/lista/japao.jpg'), null],
   ['serviço', require('./images/lista/servico.png'), null],
   ['faqueiro', require('./images/lista/faqueiro.png'), 'cutipol'],
-  ['copos', require('./images/lista/copos.jpg'), 'cutipol'],
+  ['copos', require('./images/lista/copos.jpg'), 'oferecido'],
   ['copos cocktail', require('./images/lista/cocktail.png'), 360], // 15€ copo, 4 copos conjunto, 6 conjuntos
   ['6 cadeiras de jantar', require('./images/lista/cadeiras.png'), 'oferecido'], // 150 * 6
   ['candeeiro de tecto', require('./images/lista/candeeiro.png'), 480], // flos 480
