e0e2a983646273d54577e20ae7421f314a90496c
Manuel Costa Reis <manuelfcreis@gmail.com>
2019-09-18 15:20:49 +0100

    candeeiro de tecto oferecido

--- src/list.js
+++ src/list.js
@@ -5,7 +5,7 @@ const list = [
   ['copos', require('./images/lista/copos.jpg'), 'oferecido'],
   ['copos cocktail', require('./images/lista/cocktail.png'), 360], // 15€ copo, 4 copos conjunto, 6 conjuntos
   ['6 cadeiras de jantar', require('./images/lista/cadeiras.png'), 'oferecido'], // 150 * 6
-  ['candeeiro de tecto', require('./images/lista/candeeiro.png'), 480], // flos 480
+  ['candeeiro de tecto', require('./images/lista/candeeiro.png'), 'oferecido'], // flos 480
   ['candeeiros de mesa', require('./images/lista/candeeiros.png'), 'oferecido'], // flos 480
   ['robô aspirador', require('./images/lista/robot.jpg'), 'oferecido'],
   ['batedeira ', require('./images/lista/kitchen.png'), 'oferecido'],
