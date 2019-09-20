b3d9ce785948dedf856a504b0d8563b19aac4f9f
Manuel Costa Reis <manuelfcreis@gmail.com>
2019-09-20 09:21:44 +0100

    adicionar morada igreja

--- src/components/casamento.js
+++ src/components/casamento.js
@@ -3,29 +3,43 @@ import React from "react";
 const nois = require('../images/nois.jpeg')
 
 const Casamento = () => {
-  return(
+  return (
     <div className='card' id='casamento'>
       <h3>
-      casamento
+        casamento
       </h3>
       <p>
-      21 de setembro de 2019 - 16h
+        21 de setembro de 2019 - 16h
       </p>
       <p>
-      O casamento celebra-se na Quinta da Torre, em Arcos (Vila do Conde), seguido de festa também na quinta. 
+        O casamento celebra-se na <strong>Igreja de São Miguel de Arcos</strong>, seguido de festa na
+        na Quinta da Torre, em Arcos (Vila do Conde), seguido de festa também na quinta.
       </p>
-      <img src={nois} alt='nós' />
       <p>
-      Rua Eng. Francisco Costa Reis, 249
+        <strong>Igreja de São Miguel de Arcos</strong><br />
+        R. da Igreja 209
       <br />
-      Arcos, Vila do Conde
+        Arcos, Vila do Conde
       <br />
-      <a
+        <a
+          target='_blank' rel="noopener noreferrer"
+          href='https://goo.gl/maps/5wbgaqLb97hFnSs57'>
+          Mapa
+      </a>
+      </p>
+      <p>
+        <strong>Quinta da Torre</strong><br />
+        Rua Eng. Francisco Costa Reis, 249
+      <br />
+        Arcos, Vila do Conde
+      <br />
+        <a
           target='_blank' rel="noopener noreferrer"
           href='https://goo.gl/maps/YGeUWViPSouwS59G9'>
           Mapa
       </a>
       </p>
+      <img src={nois} alt='nós' />
     </div>
   )
 }
--- src/components/contactos.js
+++ src/components/contactos.js
@@ -11,19 +11,30 @@ const Contactos = () => {
         <strong>Manuel</strong> 927609298
       </p>
       <p>
-        <strong>Quinta da Torre</strong>
-        <br />
-        R. Eng. Francisco Costa Reis 249
-        <br />
-        Arcos, Vila do Conde, Portugal
-        <br />
+        <strong>Igreja de São Miguel de Arcos</strong><br />
+        R. da Igreja 209
+      <br />
+        Arcos, Vila do Conde
+      <br />
+        <a
+          target='_blank' rel="noopener noreferrer"
+          href='https://goo.gl/maps/5wbgaqLb97hFnSs57'>
+          Mapa
+      </a>
+      </p>
+      <p>
+        <strong>Quinta da Torre</strong><br />
+        Rua Eng. Francisco Costa Reis, 249
+      <br />
+        Arcos, Vila do Conde
+      <br />
         <a
           target='_blank' rel="noopener noreferrer"
           href='https://goo.gl/maps/YGeUWViPSouwS59G9'>
           Mapa
-        </a>
+      </a>
       </p>
-      <iframe 
+      <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1082.2675593157362!2d-8.654913519877883!3d41.39637029827904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd245ad60b48d161%3A0x2d5b8008ba0ee9b2!2sR.+Eng.+Francisco+Costa+dos+Reis+249%2C+Arcos!5e1!3m2!1sen!2spt!4v1561035493902!5m2!1sen!2spt"
         height="400"
         id='map'
